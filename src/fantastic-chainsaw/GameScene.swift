3e8890fef9dd16a921f37a7d722fa29701f960f1
Ben Zobrist <ben@zobie.com>
2019-08-22 21:53:18 -0600

    Left+Right Paddle Movement
    Can move up/down, but can still leave boundries

--- src/fantastic-chainsaw/GameScene.swift
+++ src/fantastic-chainsaw/GameScene.swift
@@ -18,6 +18,12 @@ class GameScene: SKScene {
     var LeftLbl = SKLabelNode()
     var RightLbl = SKLabelNode()
     
+    // Left and Right Movement Variables
+    var LUp = false
+    var LDown = false
+    var RUp = false
+    var RDown = false
+    
     override func sceneDidLoad() {
         Ball = self.childNode(withName: "Ball") as! SKSpriteNode
         LeftPlayer = self.childNode(withName: "LeftPlayer") as! SKSpriteNode
@@ -36,20 +42,60 @@ class GameScene: SKScene {
         
     }
     
+    override func keyUp(with event: NSEvent) {
+        switch event.keyCode {
+        case 1:
+            print("leftdown")
+            LDown = false
+        case 13:
+            print("leftup")
+            LUp = false
+        case 126:
+            print("rightup")
+            RUp = false
+        case 125:
+            print("rightdown")
+            RDown = false
+        default:
+            print(event.keyCode)
+        }
+    }
+    
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
-            case 1:
-                print("down")
-            case 13:
-                print("up")
-            
-            default:
-                print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
+        case 1:
+            print("leftdown")
+            LDown = true
+        case 13:
+            print("leftup")
+            LUp = true
+        case 126:
+            print("rightup")
+            RUp = true
+        case 125:
+            print("rightdown")
+            RDown = true
+        default:
+            print(event.keyCode)
         }
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-
+        
+        // ===== Move the players based on their booleans =====
+        if LDown { // DOWN
+            LeftPlayer.run(SKAction.moveTo(y: LeftPlayer.position.y-20, duration: 0.1))
+        }
+        else if LUp { // UP
+            LeftPlayer.run(SKAction.moveTo(y: LeftPlayer.position.y+20, duration: 0.1))
+        }
+        
+        if RDown { // DOWN
+            RightPlayer.run(SKAction.moveTo(y: RightPlayer.position.y-20, duration: 0.1))
+        } else if RUp { // UP
+            RightPlayer.run(SKAction.moveTo(y: RightPlayer.position.y+20, duration: 0.1))
+        }
     }
 }
