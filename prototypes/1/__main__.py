a0f0b76530ff4f6d465ec1d52a4e01b5e9a3d4cf
Ben Zobrist <ben@zobie.com>
2019-08-22 21:00:00 -0600

    Removed Python
    Didn't even notice it was here TBH

--- prototypes/1/__main__.py
+++
@@ -1,8 +0,0 @@
-import arcade
-from proto_game import Game
-
-height = 400
-width = 800
-
-window = Game(width, height, "Fantastic Chainsaw")
-arcade.run()
--- prototypes/2/__main__.py
+++
@@ -1,8 +0,0 @@
-import arcade
-from proto_game import Game
-
-height = 600
-width = 800
-
-window = Game(width, height, "Fantastic Chainsaw")
-arcade.run()
